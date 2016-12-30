3318e909d3a0d29572d7a43e8552687952b8d1ee
aqkhan <aqk.neo@gmail.com>
2016-12-31 03:08:04 +0500

    Using MUTATORS to manipulate data before stroing in DB.

--- app/FormPost.php
+++ app/FormPost.php
@@ -10,4 +10,13 @@ class FormPost extends Model
     protected $fillable = [
         'title'
     ];
+
+    // Defining a mutator below
+    // Mutator manipulate data before storing it in database
+    // Has same format for naming convention as Accessor with a change of "set" instead "get"
+    // An example below
+
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = strtoupper($value);
+    }
 }
